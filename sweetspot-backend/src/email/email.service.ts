c13c27483908f749b96265853c6835335d5d2d72
Balint Laszlo <balint.laszlo@t-systems.com>
2020-08-26 19:05:12 +0200

    Add dynamic properties to email template.

--- sweetspot-backend/src/email/email.service.ts
+++ sweetspot-backend/src/email/email.service.ts
@@ -29,22 +29,35 @@ export class EmailService {
     <tbody>
       <tr>
         <td width="100%">
-          <div style="max-width:600px;margin:0 auto">
+          <div style="max-width: 1200px; margin: 0 auto">
             <table
               align="center"
               cellpadding="0"
               cellspacing="0"
-              style="border-spacing:0;font-family:gt-eesti,ArialMT,Helvetica,Arial,sans-serif;Margin:0 auto;padding:24px;width:100%;max-width:500px"
+              style="
+                border-spacing: 0;
+                font-family: gt-eesti, ArialMT, Helvetica, Arial, sans-serif;
+                margin: 0 auto;
+                padding: 24px;
+                width: 100%;
+                max-width: 800px;
+              "
             >
               <tbody>
                 <tr>
                   <td>
-                    <table style="margin-bottom:40px;width:100%" width="100%">
+                    <table style="margin-bottom: 40px; width: 100%" width="100%">
                       <tbody>
                         <tr>
                           <td>
-                            <a href="https://sweetspot.rs/" style="font-family:Helvetica,Arial,sans-serif;color:#0086bf" target="_blank">
-                              <img src="https://sweetspot.rs/sites/all/themes/ss/images/logo.png" style="display:block" alt="SweetSpot" width="200" height="200" />
+                            <a href="https://sweetspot.rs/" style="font-family: Helvetica, Arial, sans-serif; color: #0086bf" target="_blank">
+                              <img
+                                src="https://sweetspot.rs/sites/all/themes/ss/images/logo.png"
+                                style="display: block; margin: auto"
+                                alt="SweetSpot"
+                                width="200"
+                                height="200"
+                              />
                             </a>
                           </td>
                         </tr>
@@ -53,18 +66,17 @@ export class EmailService {
                   </td>
                 </tr>
                 <tr>
-                  <td style="text-align:justify;word-break:break-word">
-                    <table style="margin-bottom:20px;width:100%" width="100%">
+                  <td style="text-align: justify; word-break: break-word">
+                    <table style="margin-bottom: 20px; width: 100%" width="100%">
                       <tbody>
                         <tr>
                           <td>
-                            <table style="width:100%;margin-bottom:20px" width="100%" cellpadding="0" cellspacing="0">
+                            <table style="width: 100%; margin-bottom: 20px" width="100%" cellpadding="0" cellspacing="0">
                               <tbody>
                                 <tr>
-                                  <td>
-                                    <strong style="font-family:Helvetica,Arial,sans-serif;color:#234567"></strong>
-  
-                                    <h1 style="font-size:26px;line-height:30px;color:#054752;word-break:normal">
+                                  <td style="text-align: center; vertical-align: middle">
+                                    <strong style="font-family: Helvetica, Arial, sans-serif; color: #234567"></strong>
+                                    <h1 style="font-size: 26px; line-height: 30px; color: #054752; word-break: normal">
                                       Üdvözöljük! Sikeresen rendelt a SweetSpot oldalán keresztül!
                                     </h1>
                                   </td>
@@ -77,25 +89,44 @@ export class EmailService {
                           <td>
                             <center>
                               <table
-                                style="background-color:#fff;margin-bottom:20px;table-layout:fixed"
+                                style="background-color: #fff; margin-bottom: 20px; table-layout: fixed"
                                 align="center"
                                 width=""
                                 cellspacing="0"
                                 cellpadding="0"
                               >
                                 <tbody>
+                                  <tr
+                                    style="
+                                      background-color: #00aff5;
+                                      color: #fff;
+                                      text-align: center;
+                                      border-radius: 48px;
+                                      padding: 16px 24px;
+                                      border-color: transparent;
+                                      font-weight: bold;
+                                      font-size: 16px;
+                                      line-height: 1;
+                                    "
+                                  >
+                                    <th>Név</th>
+                                    <th>Telefon</th>
+                                    <th>E-mail</th>
+                                    <th>Cím</th>
+                                    <th>Végösszeg</th>
+                                    <th>Kézbesítés</th>
+                                    <th>Átvétel</th>
+                                    <th>Megjegyzés</th>
+                                  </tr>
                                   <tr>
-                                    <td
-                                      style="background-color:#00aff5;color:#fff;text-align:center;border-radius:48px;padding:16px 24px;border-color:transparent;font-weight:bold;font-size:16px;line-height:1"
-                                    ></td>
-                                    <td>Név: ${name}></td>
-                                    <td>Telefon: ${phone}</td>
-                                    <td>E-mail: ${email}</td>
-                                    <td>Cím: ${address}</td>
-                                    <td>Végösszeg: ${grandTotal} dinár</td>
-                                    <td>Kézbesítési nap: ${deliveryDate}</td>
-                                    <td>Kézbesítési forma: ${delivery}</td>
-                                    <td>Megjegyzés: ${notes}</td>
+                                    <td>${name}</td>
+                                    <td>${phone}</td>
+                                    <td>${email}</td>
+                                    <td>${address}</td>
+                                    <td>${grandTotal}</td>
+                                    <td>${deliveryDate}</td>
+                                    <td>${delivery}</td>
+                                    <td>${notes}</td>
                                   </tr>
                                 </tbody>
                               </table>
@@ -111,11 +142,11 @@ export class EmailService {
                 </tr>
                 <tr>
                   <td>
-                    <table width="100%" style="margin-bottom:20px;width:100%">
+                    <table width="100%" style="margin-bottom: 20px; width: 100%">
                       <tbody>
                         <tr>
                           <td width="100%">
-                            <div style="width:100%;height:1px;background-color:#ddd" color="#DDD" width="100%"></div>
+                            <div style="width: 100%; height: 1px; background-color: #ddd" color="#DDD" width="100%"></div>
                           </td>
                         </tr>
                       </tbody>
@@ -123,37 +154,33 @@ export class EmailService {
                   </td>
                 </tr>
                 <tr>
-                  <td style="text-align:center">
+                  <td style="text-align: center">
                     <img
                       src="https://sweetspot.rs/sites/all/themes/ss/images/logo.png"
                       alt="SweetSpot"
-                      style="display:block;width:29px;height:auto;margin-left:auto;margin-right:auto;margin-bottom:10px"
+                      style="display: block; width: 29px; height: auto; margin-left: auto; margin-right: auto; margin-bottom: 10px"
                       height="auto"
                     />
                   </td>
                 </tr>
                 <tr>
-                  <td style="text-align:center;font-size:13px">
-                    <a href="#" style="color:#00aff5" target="_blank">
-                      SweetSpot Rendelés
-                    </a>
-                    <span style="color:#00aff5">|</span>
-                    <a href="https://sweetspot.rs/" style="color:#00aff5" target="_blank">
-                      SweetSpot Blog
-                    </a>
+                  <td style="text-align: center; font-size: 13px">
+                    <a href="#" style="color: #00aff5" target="_blank"> SweetSpot Rendelés </a>
+                    <span style="color: #00aff5">|</span>
+                    <a href="https://sweetspot.rs/" style="color: #00aff5" target="_blank"> SweetSpot Blog </a>
                   </td>
                 </tr>
                 <tr>
-                  <td style="text-align:center">
+                  <td style="text-align: center">
                     <table
-                      style="max-width:100%;width:100%;text-align:center;font-family:ArialMT,Arial,sans-serif"
+                      style="max-width: 100%; width: 100%; text-align: center; font-family: ArialMT, Arial, sans-serif"
                       cellspacing="0"
                       cellpadding="0"
                     >
                       <tbody>
                         <tr>
-                          <td style="text-align:center">
-                            <p style="font-size:10px;color:#708c91;text-align:center;padding:0;margin-top:10px;margin-bottom:2px">
+                          <td style="text-align: center">
+                            <p style="font-size: 10px; color: #708c91; text-align: center; padding: 0; margin-top: 10px; margin-bottom: 2px">
                               Az e-mail üzenetet sweetspot.subotica@gmail.com címen keresztül kapta meg
                             </p>
                           </td>
