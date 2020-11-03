eb0b735be8726ff45d1c652bb2a5aa1b77066dfb
Balint Laszlo <balint.laszlo@t-systems.com>
2020-11-03 15:35:54 +0100

    Document editable table stuff

---
+++ sweetspot-frontend/src/containers/Management/Management.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Management = (props) => <div></div>;
+
+export default Management;
+
+// https://www.npmjs.com/package/react-image-crop
+
+// <input value={props.row.values.name} onBlur={() => clicked(props.row.values)} />
